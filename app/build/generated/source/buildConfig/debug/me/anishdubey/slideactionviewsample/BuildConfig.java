10a925b034e20d378bef3b66aea84acf02d75577
anish <anish.dubey@citymall.live>
2022-02-12 09:17:36 +0530

    init

---
+++ app/build/generated/source/buildConfig/debug/me/anishdubey/slideactionviewsample/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.anishdubey.slideactionviewsample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "me.anishdubey.slideactionviewsample";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "0.0.2";
+}
--- app/src/main/java/me/anishdubey/slideactionviewsample/MainActivity.java
+++ app/src/main/java/me/anishdubey/slideactionviewsample/MainActivity.java
@@ -5,30 +5,31 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
 import me.anishdubey.slideactionview.SlideActionListener;
 import me.anishdubey.slideactionview.SlideActionView;
-import me.jfenn.slideactionviewsample.R;
+import me.anishdubey.slideactionviewsample.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SlideActionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        SlideActionView view = findViewById(R.id.actionView);
-//        view.setListener(this);
-//        view.setLeftIcon(ContextCompat.getDrawable(this, R.drawable.ic_announcement_black_24dp));
-//        view.setRightIcon(ContextCompat.getDrawable(this, R.drawable.ic_assignment_turned_in_black_24dp));
+        SlideActionView view = findViewById(R.id.actionView);
+        view.setListener(this);
+        view.setLeftIcon(ContextCompat.getDrawable(this, R.drawable.ic_announcement_black_24dp));
+        view.setRightIcon(ContextCompat.getDrawable(this, R.drawable.ic_assignment_turned_in_black_24dp));
     }
 
-//    @Override
-//    public void onSlideLeft() {
-//        Toast.makeText(this, "slided left side", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    public void onSlideRight() {
-//        Toast.makeText(this, "slided right side", Toast.LENGTH_SHORT).show();
-//    }
+    @Override
+    public void onSlideLeft() {
+        Toast.makeText(this, "slided left side", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSlideRight() {
+        Toast.makeText(this, "slided right side", Toast.LENGTH_SHORT).show();
+    }
 }
